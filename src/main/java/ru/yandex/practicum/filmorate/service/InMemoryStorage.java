2a9705c95f448c0eafc9cc2bef5e1d711c540597
RuslanYapparov <117994991+RuslanYapparov@users.noreply.github.com>
2023-03-26 21:43:31 +0400

    fix: Добавить строку для прохождения проверки

--- src/main/java/ru/yandex/practicum/filmorate/service/InMemoryStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/InMemoryStorage.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.service;
 import java.util.List;
 // Приложение позволяет хранить валидные объекты двух типов. Для хранения данных в RAM используется HashMap
 // Данный интерфейс по сути переназывает методы интерфеса Map + добавляет функцию предоставления id для сохранения
+
 public interface InMemoryStorage<T> {
 
     Integer produceId();
