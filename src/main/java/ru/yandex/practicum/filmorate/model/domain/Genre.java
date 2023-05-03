becebfd773e06a0b04fc9cb553c9c2bc7c6db781
Ruslan Yapparov <RuYappy@yandex.ru>
2023-05-04 00:13:10 +0400

    style: Исправить чек-стайл (пробелы между конмтантами и значениями их полей)

--- src/main/java/ru/yandex/practicum/filmorate/model/domain/Genre.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/domain/Genre.java
@@ -5,12 +5,12 @@ import ru.yandex.practicum.filmorate.exception.ObjectNotFoundInStorageException;
 @lombok.AllArgsConstructor
 @lombok.Getter
 public enum Genre {
-    COMEDY (1, "Комедия"),
-    DRAMA (2, "Драма"),
-    CARTOON (3, "Мультфильм"),
-    THRILLER (4, "Триллер"),
-    DOCUMENTARY (5, "Документальный"),
-    ACTION (6, "Боевик");
+    COMEDY(1, "Комедия"),
+    DRAMA(2, "Драма"),
+    CARTOON(3, "Мультфильм"),
+    THRILLER(4, "Триллер"),
+    DOCUMENTARY(5, "Документальный"),
+    ACTION(6, "Боевик");
 
     private final int id;
     private final String byRus;
--- src/main/java/ru/yandex/practicum/filmorate/model/domain/RatingMpa.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/domain/RatingMpa.java
@@ -5,11 +5,11 @@ import ru.yandex.practicum.filmorate.exception.ObjectNotFoundInStorageException;
 @lombok.AllArgsConstructor
 @lombok.Getter
 public enum RatingMpa {
-    G (1, "G"),
-    PG (2, "PG"),
-    PG_13 (3, "PG-13"),
-    R (4, "R"),
-    NC_17 (5, "NC-17");
+    G(1, "G"),
+    PG(2, "PG"),
+    PG_13(3, "PG-13"),
+    R(4, "R"),
+    NC_17(5, "NC-17");
 
     private final int id;
     private final String name;
